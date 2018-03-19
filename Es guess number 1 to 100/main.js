caaaa1a391c82a2915fa9abdcd5dc049d1fdea69
DavideL90 <davide.lecci@icloud.com>
2018-03-19 16:35:07 +0100

    finito

--- Es guess number 1 to 100/main.js
+++ Es guess number 1 to 100/main.js
@@ -26,16 +26,29 @@ console.log(bomb_Number);
 number_found = false; //variabile che indica se l'utente "becca" un numero vietato;
 count_attempts = 0; //Conto i tentativi fatti dall'utente;
 used_numbers = []; //Creo un array affinché l'utente non riusi un numero.
+console.log(used_numbers);
 do {
   if(difficulty == 0){
     var number_guess = prompt("Inserisci un numero da 1 a 100:"); //Numero inserito dall'utente;
+    number_guess = parseInt(number_guess); //trasformo in int il prompt sennò includes non funziona;
   }
   else if(difficulty == 1){
     var number_guess = prompt("Inserisci un numero da 1 a 80");
+    number_guess = parseInt(number_guess);
   }
   else{
     var number_guess = prompt("Inserisci un numero da  1 a 50");
+    number_guess = parseInt(number_guess);
   }
+  //Controllo che il numero non sia già stato inserito;
+  if((used_numbers.includes(number_guess)) || (count_attempts != 0)){
+    number_guess = prompt("Inserisci un nuovo numero, il precedente risulta già utilizzato");
+    number_guess = parseInt(number_guess);
+  }
+  else{
+    used_numbers.push(number_guess);
+  }
+  console.log(used_numbers);
   console.log(number_guess);
   if(bomb_Number.includes(number_guess)){
     number_found = true;
@@ -45,10 +58,3 @@ do {
 } while(number_found == false);
 
 document.write("Numero tentativi : " + count_attempts);
-// do{
-//   if(number_guess == bomb_Number[i]){
-//       number_found = true;
-//       console.log(number_found);
-//   }
-//   i++;
-// }while((number_found == false) && (i < bomb_Number.length));
